c0a503b774527500a510cf7e6600f1461cbf4069
Kevin Tourret <tourret.kevin@gmail.com>
2021-02-05 16:08:53 +0100

    Add more tests

--- tests/ArtistControllerTest.php
+++ tests/ArtistControllerTest.php
@@ -4,7 +4,11 @@
 namespace App\Tests;
 
 
+use App\Entity\Artist;
+use App\Repository\ArtistRepository;
+use Doctrine\ORM\QueryBuilder;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Class ArtistControllerTest.php
@@ -55,8 +59,52 @@ class ArtistControllerTest extends WebTestCase
             ->eq(0)
             ->link();
 
-        $this->assertNotEmpty($linkCreateNew, 'Link crerate new not found');
+        $this->assertNotEmpty($linkCreateNew, 'Link create new not found');
         // On vérifit que le lien est cliquable par le "client" et que la page sur laquelle on est redirigé existe
         $this->assertNotNull($client->click($linkCreateNew), 'Error while trying to access /artiste/new');
     }
+
+    public function testIndexFormFilter() {
+        // Créer "virtuellement" votre site
+        $client = static::createClient();
+        // Je dis à mon site, d'accéder à l'url /artiste
+        $crawler = $client->request('GET', '/artiste/');
+
+        // On récupère le formulaire lié au bouton 'Filtrer'
+        $form = $crawler->selectButton('Filtrer')->form();
+        $form['artists_filter[beginningYear]'] = '1960';
+        $crawler = $client->submit($form);
+        $this->assertNotEmpty($crawler, 'On a pas ete redirige apres avoir soumis le form');
+    }
+
+    public function testShowArtist() {
+        // Créer "virtuellement" votre site
+        $client = static::createClient();
+        // Je dis à mon site, d'accéder à l'url /artiste
+        $crawler = $client->request('GET', '/artiste/');
+        // Appel vers la fonction de récupération des artistes
+        $artists = $this->getArtists();
+
+        foreach ($artists as $artist) {
+            /** @var Artist $artist */
+            $urlArtist = '/artiste/' . $artist->getId();
+            // On récupère un lien dont le href vaut le l'url du show artiste
+            $link = $crawler->filter('a[href="' . $urlArtist . '"]')
+                ->eq(0)
+                ->link();
+            // On vérifit si le lien existe
+            $this->assertNotEmpty($link, 'Le lien vers le detail de l artiste n a pas ete trouve');
+        }
+    }
+
+    public function getArtists() {
+        // On récupère le repository des artistes
+        $artistsRepository = static::$container->get(ArtistRepository::class);
+        // On récupère les artistes de l'app et on les renvoit
+        $qb = $artistsRepository->queryAll();
+        /** @var QueryBuilder $qb */
+        return $qb->setMaxResults('10')->getQuery()->getResult();
+    }
+
+
 }
